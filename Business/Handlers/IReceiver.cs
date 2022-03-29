0121fbe6284744a3661aef98d616dc9e47ff9594
jebarcha <jebarcha@hotmail.com>
2022-03-29 11:31:24 -0600

    Chain or responsibility - Payment demo improved

---
+++ Business/Handlers/IReceiver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment_processing.Business.Handlers
+{
+    public interface IReceiver<T> where T : class
+    {
+        void Handle(T request);
+    }
+}
